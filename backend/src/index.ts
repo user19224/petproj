d76d7a7bf40d2a9a04d428fac773549bf520ba9c
user19224 <mgleb914@gmail.com>
2025-05-01 17:43:50 +0300

    added simple trpc endpoint on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,40 +1,25 @@
 import express from 'express'
-import {initTRPC} from '@trpc/server'
-
+import * as trpcExpress from '@trpc/server/adapters/express'  
+import { trpcRouter } from './trpc'
 
 const expressApp = express()
-const ideas = [
-    {
-      nick: 'nickname1',
-      name: 'Idea 1',
-      description: 'Description of idea 1...',                                                                                                           },
-    {
-      nick: 'nickname2',
-      name: 'Idea 2',
-      description: 'Description of idea 2...',
-    },
-    {
-      nick: 'nickname3',
-      name: 'Idea 3',
-      description: 'Description of idea 3...',
-    },
-    {
-      nick: 'nickname4',
-      name: 'Idea 4',
-      description: 'Description of idea 4...',
-    },
-    {
-      nick: 'nickname5',
-      name: 'Idea 5',
-      description: 'Description of idea 5...',
-    },
-  ]
 
-expressApp.get('/ideas',(req,res)=>{
-  
-    res.send(ideas)
-})
+
+
+expressApp.use(
+  '/trpc',
+  trpcExpress.createExpressMiddleware({
+    router: trpcRouter,
+  })
+
+
+)
+
+
+
+
+
 
 expressApp.listen(3000,()=>{
-  console.info("Listening at https://localhost:3000")
+  console.info("Listening at http://localhost:3000")
 })
